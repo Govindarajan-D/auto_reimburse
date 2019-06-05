9b570f95736103f1f116358a749b8097ccd33198
ramgopalan <48012333+ramgopalan@users.noreply.github.com>
2019-06-05 23:43:07 +0530

    Included the Python code for Selectors
    Cleaned and Indented

--- zohoextension.py
+++ zohoextension.py
@@ -4,11 +4,11 @@ options = webdriver.ChromeOptions()
 options.add_argument(r"user-data-dir=C:\Users\\" + getpass.getuser() + r"\AppData\Local\Google\Chrome\User Data")
 driver = webdriver.Chrome(options=options)
 driver.get('https://people.zoho.com/visualbisolutions/zp#reimbursement/form/add-formLinkName:Reimbursement_Form')
-invoice_amount = driver.find_element_by_id("zp_field_249048000002801079")
+
+/** Fixed Values **/
 invoice_amount = driver.find_element_by_name("Currency_11")
 invoice_amount.clear()
 invoice_amount.send_keys("1000")
-from_date = driver.find_element_by_id("zp_field_249048000003005331")
 from_date = driver.find_element_by_name("Claim_From")
 from_date.clear()
 from_date.send_keys("01-Jun-2019")
@@ -18,11 +18,14 @@ to_date.send_keys("30-Jun-2019")
 invoice_date = driver.find_element_by_name("Date1")
 invoice_date.clear()
 invoice_date("5-Jun-2019")
-expense_type = driver.find_element_by_id("s2id_zp_field_249048000002801091")
-expense_type.send_keys("Phone & Internet")
-
 description = driver.find_element_by_name("Description")
 description.send_keys("Dummy")
 final_amount = driver.find_element_by_name("Currency_1")
 final_amount.clear()
 final_amount.send_keys("1000")
+
+/** Selectors **/
+driver.find_element_by_xpath("//select[@name='Expense_Type']/option[text()='Phone & Internet']").click()
+driver.find_element_by_xpath("//select[@name='Proof_Attached']/option[text()='No']").click()
+
+
