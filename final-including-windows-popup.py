f4dae8356737fdc396acd7ef0f5ba11b0ad37ba1
ramgopalan <48012333+ramgopalan@users.noreply.github.com>
2019-06-07 16:10:03 +0530

    Windows and Zoho Update
    Featured - File Upload and Auto Fill in Zoho

---
+++ final-including-windows-popup.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import getpass
+import pywinauto
+options = webdriver.ChromeOptions()
+options.add_argument(r"user-data-dir=C:\Users\\" + getpass.getuser() + r"\AppData\Local\Google\Chrome\User Data")
+driver = webdriver.Chrome(options=options)
+driver.get('https://people.zoho.com/visualbisolutions/zp#reimbursement/form/add-formLinkName:Reimbursement_Form')
+
+# Fixed Values
+invoice_amount = driver.find_element_by_name("Currency_11")
+invoice_amount.clear()
+invoice_amount.send_keys("1000")
+from_date = driver.find_element_by_name("Claim_From")
+from_date.clear()
+from_date.send_keys("01-Jun-2019")
+to_date = driver.find_element_by_name("Claim_To")
+to_date.clear()
+to_date.send_keys("30-Jun-2019")
+invoice_date = driver.find_element_by_name("Date1")
+invoice_date.clear()
+invoice_date("5-Jun-2019")
+description = driver.find_element_by_name("Description")
+description.send_keys("Dummy")
+final_amount = driver.find_element_by_name("Currency_1")
+final_amount.clear()
+final_amount.send_keys("1000")
+
+# Selectors
+driver.find_element_by_xpath("//select[@name='Expense_Type']/option[text()='Phone & Internet']").click()
+driver.find_element_by_xpath("//select[@name='Proof_Attached']/option[text()='No']").click()
+
+# File Upload
+click_upload = driver.find_element_by_name("Proof_Attachement_zip")
+
+# Windows Interaction with Pop up
+app = pywinauto.application.Application()
+mainWindow = app['Open'] # main windows' title
+ctrl=mainWindow['Edit'] 
+ctrl.ClickInput()
+ctrl.TypeKeys('dummy.txt') 
+ctrlBis = mainWindow['Open'] # open file button
+ctrlBis.ClickInput()
